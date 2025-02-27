d6e2651e88640901d598a7577fa297739c79b025
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-27 16:07:20 +0500

    feat: add ButtonSection component, add styling

---
+++ src/components/ButtonSection.jsx
@@ -0,0 +1,12 @@
+import '../styles/ButtonSection.css'
+
+
+export default function ButtonSection() {
+    return <article className='button-article'>
+                <section className='button-section'>
+                  <h3>Ready for a recipe?</h3>  
+                  <p>Generate a recipe from your list of ingredients.</p>
+                </section>
+                <button>Get a recipe</button>
+           </article>
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
 import '../styles/styles.css'
 import Form from './Form'
 import TextSection from './TextSection'
+import ButtonSection from './ButtonSection'
 
 
 export default function Main() {
     return <main className='main'>
                 <Form />
                 <TextSection />
+                <ButtonSection />
            </main>
 }
