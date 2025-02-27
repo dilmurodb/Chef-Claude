639c759f9d2832e701dab631cfefd6e882fddb0c
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-27 15:48:58 +0500

    feat: add TextSection component and add styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,11 @@
 import '../styles/styles.css'
 import Form from './Form'
+import TextSection from './TextSection'
 
 
 export default function Main() {
     return <main className='main'>
                 <Form />
+                <TextSection />
            </main>
 }
---
+++ src/components/TextSection.jsx
@@ -0,0 +1,17 @@
+import '../styles/TextSection.css'
+
+
+export default function TextSection() {
+    return <section className='text-section'>
+                <h2>Ingredients on hand:</h2>
+                <ul>
+                    <li>Chicken breasts</li>
+                    <li>Most of the main spices</li>
+                    <li>Olive oil</li>
+                    <li>Heavy cream</li>
+                    <li>Chicken broth</li>
+                    <li>Parmesan cheese</li>
+                    <li>Spinach</li>
+                </ul>
+           </section>
+}
