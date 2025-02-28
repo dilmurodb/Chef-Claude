9b7725aa162afda1fa4cf7e4d6182a367166cb28
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-28 13:29:00 +0500

    refactor: delete commented out code

--- src/components/TextSection.jsx
+++ src/components/TextSection.jsx
@@ -8,13 +8,6 @@ export default function TextSection( { ingredientsList }) {
                     {
                         ingredientsList.map((ingredient, index) => <li key={index}>{ingredient}</li>)
                     }
-                    {/* <li>Chicken breasts</li>
-                    <li>Most of the main spices</li>
-                    <li>Olive oil</li>
-                    <li>Heavy cream</li>
-                    <li>Chicken broth</li>
-                    <li>Parmesan cheese</li>
-                    <li>Spinach</li> */}
                 </ul>
            </section>
 }
